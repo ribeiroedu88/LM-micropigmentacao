a9bce9dd5da2bb7b96dd45af1479ebc7306bf57f
Eduardo Ribeiro <95627481+ribeiroedu88@users.noreply.github.com>
2022-04-29 15:18:48 -0300

    alt

---
+++ js/style.js
@@ -0,0 +1,10 @@
+////////////////////CONFIGURAÇÃO DO MENU MOBILE/////////////////////
+
+const btnMobile = document.getElementById('btn-mobile')
+
+    function toggleMenu() {
+        const nav = document.getElementById('nav')
+        nav.classList.toggle('active')
+    }
+
+btnMobile.addEventListener('click', toggleMenu)
